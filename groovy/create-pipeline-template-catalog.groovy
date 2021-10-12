ceac6286e4f2f9241961c7ecb25a8852db5bb1db
Logan Donley <ldonley@cloudbees.com>
2021-10-12 16:07:51 -0400

    Adding insurance app

--- groovy/create-pipeline-template-catalog.groovy
+++ groovy/create-pipeline-template-catalog.groovy
@@ -70,6 +70,48 @@ if (microBlogJob == null) {
   def p = jenkins.createProjectFromXML(name, new ByteArrayInputStream(frontendJobXml.getBytes("UTF-8")));
 
   logger.info("created $name job")
+
+ def frontendInsuranceXML = """
+  <org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject plugin="workflow-multibranch@2.21">
+    <properties>
+      <com.cloudbees.pipeline.governance.templates.classic.multibranch.GovernanceMultibranchPipelinePropertyImpl plugin="cloudbees-workflow-template@3.7">
+        <instance>
+        <model>cb-demo/react-app</model>
+        <values class="tree-map">
+          <entry>
+            <string>gcpProject</string>
+            <string>core-flow-research</string>
+          </entry>
+          <entry>
+            <string>githubCredentialId</string>
+            <string>github-sa</string>
+          </entry>
+          <entry>
+            <string>name</string>
+            <string>Insurance Frontend</string>
+          </entry>
+          <entry>
+            <string>repoOwner</string>
+            <string>REPLACE_GITHUB_ORG</string>
+          </entry>
+          <entry>
+            <string>repository</string>
+            <string>insurance-frontend</string>
+          </entry>
+        </values>
+      </instance>
+      </com.cloudbees.pipeline.governance.templates.classic.multibranch.GovernanceMultibranchPipelinePropertyImpl>
+    </properties>
+    <factory class="com.cloudbees.pipeline.governance.templates.classic.multibranch.FromTemplateBranchProjectFactory" plugin="cloudbees-workflow-template@3.7">
+      <owner class="org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject" reference="../.."/>
+      <catalogName>cb-demo</catalogName>
+      <templateDirectory>react-app</templateDirectory>
+    </factory>
+  </org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject>
+  """
+
+  def p = jenkins.createProjectFromXML("Insurance Frontend", new ByteArrayInputStream(frontendInsuranceXML.getBytes("UTF-8")));
+
   def backendName = "microblog-backend"
   //microblog-backend job from Pipeline Template
   def backendJobXml = """
@@ -114,6 +156,48 @@ if (microBlogJob == null) {
   def backendProject = jenkins.createProjectFromXML(backendName, new ByteArrayInputStream(backendJobXml.getBytes("UTF-8")));
 
   logger.info("created $backendName job")  
+
+  def insuranceBackendXML = """
+  <org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject plugin="workflow-multibranch@2.26">
+  <properties>
+    <com.cloudbees.pipeline.governance.templates.classic.multibranch.GovernanceMultibranchPipelinePropertyImpl plugin="cloudbees-workflow-template@3.12">
+      <instance>
+        <model>cb-demo/python-poetry</model>
+        <values class="tree-map">
+          <entry>
+            <string>gcpProject</string>
+            <string>core-flow-research</string>
+          </entry>
+          <entry>
+            <string>githubCredentialId</string>
+            <string>github-sa</string>
+          </entry>
+          <entry>
+            <string>name</string>
+            <string>Insurance Backend</string>
+          </entry>
+          <entry>
+            <string>repoOwner</string>
+            <string>REPLACE_GITHUB_ORG</string>
+          </entry>
+          <entry>
+            <string>repository</string>
+            <string>insurance-backend</string>
+          </entry>
+        </values>
+      </instance>
+    </com.cloudbees.pipeline.governance.templates.classic.multibranch.GovernanceMultibranchPipelinePropertyImpl>
+  </properties>
+  <factory class="com.cloudbees.pipeline.governance.templates.classic.multibranch.FromTemplateBranchProjectFactory" plugin="cloudbees-workflow-template@3.12">
+    <owner class="org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject" reference="../.." />
+    <catalogName>cb-demo</catalogName>
+    <templateDirectory>python-poetry</templateDirectory>
+  </factory>
+</org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject>
+  """
+
+  def backendProject = jenkins.createProjectFromXML("Insurance Backend", new ByteArrayInputStream(insuranceBackendXML.getBytes("UTF-8")));
+
   
 } else {
   logger.info("$name job already exists")
